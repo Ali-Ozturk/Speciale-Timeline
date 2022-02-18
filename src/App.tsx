822e321211feff1b4e6e7aa98da9b91c961766fc
Ali <aliozturk@outlook.dk>
2022-02-18 14:47:42 +0100

    Finalized design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {Chrono} from "react-chrono";
+import {items} from "./data/TimelineData";
+import moment from 'moment';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [isAlternating, setAlternating] = React.useState(false);
+
+    const today = moment();
+    let chosenItem = null;
+
+    items.forEach((item) => {
+        const itemDate = moment(item.title!, 'DD-MM-YYYY');
+
+        if (itemDate < today) {
+            chosenItem = item;
+        }
+    })
+
+    const foundIndex = chosenItem ? items.indexOf(chosenItem) : 0;
+
+    return (
+        <div style={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', margin: 'auto'}}>
+            <Chrono items={items}
+                    mode={isAlternating ? "VERTICAL_ALTERNATING" : "VERTICAL"}
+                    activeItemIndex={foundIndex}
+                    cardHeight={100}
+                    theme={{
+                        primary: "#6F2232",
+                        secondary: "#ffffff",
+                        cardBgColor: "#ffffff",
+                        cardForeColor: "black",
+                        titleColor: "black"
+                    }}
+                    hideControls={true}
+            />
+
+            <div style={{position: 'absolute', top: '1rem', left: '2rem'}}>
+                <button className={'alternate-button'} onClick={() => setAlternating(!isAlternating)}>Change view
+                </button>
+            </div>
+        </div>
+    )
+
 }
 
 export default App;
---
+++ src/data/TimelineData.tsx
@@ -0,0 +1,44 @@
+import {TimelineItemModel} from "react-chrono/dist/models/TimelineItemModel";
+
+export const items: TimelineItemModel[] = [
+    {
+        title: '01-02-2022',
+        cardTitle: "Projekt start",
+        cardDetailedText: "Her begyndte vi"
+    },
+    {
+        title: '28-03-2022',
+        cardTitle: "Loading af raw data",
+        cardDetailedText: "Vi skal have færdiggjort vores loading af rå data her"
+    },
+    {
+        title: '15-04-2022',
+        cardTitle: "Geodatastyrelsen data",
+        cardDetailedText: "Vi har fået at vide fra Geodatastyrelsen at vi i midten af april ca. få data vi kunne sammenligne med"
+    },
+    {
+        title: '16-05-2022',
+        cardTitle: "Færdiggør implementation",
+        cardDetailedText: "Her bør vi være færdige med implementationen af vores system"
+    },
+    {
+        title: '23-05-2022',
+        cardTitle: "Usability testing",
+        cardDetailedText: "Vi skal have testet vores system"
+    },
+    {
+        title: '03-06-2022',
+        cardTitle: "Gennemlæs og lav korrektur på artikel",
+        cardDetailedText: "Gennemlæs og lav korrektur på artikel"
+    },
+    {
+        title: '10-06-2022',
+        cardTitle: "Aflevering af projekt",
+        cardDetailedText: "SÅ SKAL DER AFLEVERES WUHUUU"
+    },
+    {
+        title: '24-06-2022',
+        cardTitle: "Speciale eksamen",
+        cardDetailedText: "To uger efter aflevering skal vi tidligst op til eksamen"
+    }
+];
